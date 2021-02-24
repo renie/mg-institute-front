71a23af545666faed0dbd99ea3673b9ae401fd85
Renie <renie.webdev@gmail.com>
2021-02-24 23:09:27 +0200

    - Basic input form

--- src/pages/components/addVideo/index.js
+++ src/pages/components/addVideo/index.js
@@ -2,9 +2,10 @@ import { render as genericRender, loadComponent } from '../../../common/componen
 import { domFind } from '../../../common/dom'
 
 import * as styles from './index.scss'
+import * as form from '../forms/index.scss'
 
 
-export const getAddVideo = async (index) => await loadComponent('addVideo', {styles, index})
+export const getAddVideo = async (index) => await loadComponent('addVideo', {styles, index, form})
 
 export const render = async (selector = '#addVideo') => {
     genericRender(await getAddVideo(domFind(selector).children.length), selector)
